09e5bd38e907bb2148389f57c4e3a6a70d16f44a
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-04-25 23:12:39 +0430

    create models in website and blog application
    +contact
    +Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    #Image
+    #author
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    #category
+    counted_views = models.IntegerField(default=0) #(default=0)
+    status = models.BooleanField(default=False)
+    puplished_date = models.BooleanField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
