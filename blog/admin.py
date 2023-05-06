6440b03e4bcef62a5ef6dfe5e916fa18d99da3d1
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-05-07 00:38:49 +0430

    change some links in blog application
    +blog_home
    +single

--- blog/admin.py
+++ blog/admin.py
@@ -5,9 +5,9 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_views', 'status', 'puplished_date','created_date')
+    list_display = ('title', 'author', 'counted_views', 'status', 'puplished_date','created_date')
     list_filter = ('status',)
-    ordering = ['-created_date']
+    #ordering = ['-created_date']
     search_fields = ['title', 'content']
 admin.site.register(Post, PostAdmin)
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+import datetime
 
 # Create your views here.
 def blog_view(request):
@@ -7,10 +8,16 @@ def blog_view(request):
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+def blog_single(request,pid):
+    posts = Post.objects.filter(status=1)
+    post = get_object_or_404(Post, pk= pid)
+    context ={'post': post}
+    return render(request, 'blog/blog-single.html',context)
 
 def test(request):
+
+    #posts = Post.objects.exclude(puplished_date__gt=datetime.datetime.now())
     posts = Post.objects.all()
-    context = {'posts': posts}
+    #time = Post.objects.dates('puplished_date','M D',order='DESC')
+    context = {'posts': posts , 'time': time}
     return render(request, 'test.html',context)
