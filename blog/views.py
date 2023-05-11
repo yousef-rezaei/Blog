fc71df594f7175441dc00261bd82046c510057e1
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-05-11 22:06:10 +0430

    create pre_post and next_post
    create pre_post and next_post in view file in blog application

--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,38 @@ def blog_view(request):
 def blog_single(request,pid):
     posts = Post.objects.filter(status=1)
     post = get_object_or_404(Post, pk= pid)
-    context ={'post': post}
+
+    #Start Update Counted_views
+    post.counted_views = post.counted_views +1
+    post.save()
+    #End Update Counted_views
+
+    #Start looking for NextPost and PrevPost
+    i = -1
+    if post != None  :
+        for n in posts:
+            i = i+1
+            if n.id == post.id:
+                break
+    
+        if i==0:
+            pre_post = posts[len(posts)-1]  
+        else:
+            pre_post= posts[i-1]
+
+        if i== (len(posts)-1):
+            next_post = posts[0]  
+        else:
+            next_post = posts[i+1]
+     #End looking for NextPost and PrevPost
+
+    context ={'post': post, 'pre_post':pre_post , 'next_post':next_post}
     return render(request, 'blog/blog-single.html',context)
 
 def test(request):
 
-    #posts = Post.objects.exclude(puplished_date__gt=datetime.datetime.now())
-    posts = Post.objects.all()
+    posts = Post.objects.exclude(puplished_date__gt=datetime.datetime.now())
+    #posts = Post.objects.all()
     #time = Post.objects.dates('puplished_date','M D',order='DESC')
     context = {'posts': posts , 'time': time}
     return render(request, 'test.html',context)
