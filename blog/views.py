b751d1794aeb2587d2b2169508adfc2a13d9aaf2
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-05-23 02:21:11 +0330

    create blog-search
    create blog_search.html and implement search part in our project

--- blog/views.py
+++ blog/views.py
@@ -52,3 +52,11 @@ def blog_category(request,cat_name):
     posts= posts.filter(category__name=cat_name)
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        if s:= request.GET.get('s'):
+            posts= posts.filter(content__contains=s)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
