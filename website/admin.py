b3b74ae189ceb91351d615fd852633e86fb47d32
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-06-08 09:33:22 +0330

    create forms with forms.py
    +contact
    +newsletter
    +urls.py

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from website.models import Contact
+from website.models import Contact, Newsletter
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ('name', 'message')
 
 admin.site.register(Contact,ContactAdmin)
+admin.site.register(Newsletter)
--- website/views.py
+++ website/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from website.models import Contact
-from website.forms import NameForm,ContactForm
+from website.forms import NameForm,ContactForm, NewsletterForm
 
 def index_view(request):
     return render(request, 'website/index.html')
 
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = ContactForm()
+    return render(request,'website/contact.html',{'form':form})
 
 
 def about_view(request):
     return render(request,'website/about.html')
-
 
 def test_view(request):
     if request.method == 'POST':
@@ -26,3 +30,13 @@ def test_view(request):
 
     form = ContactForm()
     return render(request, 'test.html',{'form': form})
+
+
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
