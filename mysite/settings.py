43f22d466683fdc893c5b3d0aa04df9e27909303
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-06-18 02:24:23 +0330

    set robots
    set robots

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,13 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'robots',
 
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blog'
 ]
 
+# sites framework
 SITE_ID = 2
+
+# robots
+ROBOTS_USE_HOST = True
+ROBOTS_USE_SITEMAP = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
