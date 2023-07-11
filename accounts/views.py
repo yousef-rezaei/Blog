11cdac087836d3752cf68bf44af1163ae87f1bbe
yousef-rezaei <yousef.rezaei9@gmail.com>
2023-07-11 13:45:13 +0330

    create Logout Form
    create logout form

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 # Create your views here.
 
@@ -21,8 +23,21 @@ def login_view(request):
     else:
         return redirect('/')
 
+@login_required
 def logout_view(request):
-    pass
+    logout(request)
+    return redirect('/')
+
 
 def signup_view(request):
-    return(request,'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method== 'POST':
+            form=UserCreationForm(request.POST)
+            if form.is_valid:
+                form.save()
+                return redirect('/')
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request,'accounts/signup.html', context)
+    else:
+        return redirect('/')
